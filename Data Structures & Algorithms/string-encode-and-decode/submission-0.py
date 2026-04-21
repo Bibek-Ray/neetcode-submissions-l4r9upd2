0e4026777f8d36a05fa3b449cd054fb9e13853ed
Bibek Ray <bibekray.dec@gmail.com>
2026-04-22 02:24:06 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def encode(self, strs: list[str]) -> str:
+        codec = ""
+        for val in strs:
+            codec += str(len(val)) + '#' + val
+
+        return codec
+
+    def decode(self, s: str) -> list[str]:
+        codec = []
+        start = 1
+        k = ""
+        i = 0
+        while i < len(s):
+            if s[i] != '#' and s[i].isdigit():
+                k += s[i]
+                i += 1
+            else:
+                i += 1
+                codec.append(s[i: i + int(k)])
+                i += int(k)
+                k = ""
+
+        return codec
